a031588c03ceb9330f8132a17389f3a050eb6ff1
ondrejd <ondrejd@gmail.com>
2015-09-19 13:19:11 +0200

    Disabled XDebug for PHPUnit tests.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -9,5 +9,12 @@
 
 chdir(dirname(__DIR__));
 
+ini_set('xdebug.default_enable', false);
+ini_set('xdebug.remote_enable', false);
+ini_set('xdebug.auto_trace', false);
+ini_set('xdebug.show_exception_trace', false);
+ini_set('xdebug.coverage_enable', false);
+ini_set('xdebug.profiler_enable', false);
+
 require_once 'vendor/autoload.php';
 
